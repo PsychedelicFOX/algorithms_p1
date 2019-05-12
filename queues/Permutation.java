61a26d978946ae6e27c3c789d39025c98e99d2ff
psy <khristinevgeniy@gmail.com>
2019-05-12 23:50:49 +0300

    DoubleLinked deque

---
+++ queues/Permutation.java
@@ -0,0 +1,10 @@
+/**
+ * @author psy
+ * @date 5/12/19.
+ */
+public class Permutation {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        Deque<Integer> deque = new Deque<>();
+    }
+}
